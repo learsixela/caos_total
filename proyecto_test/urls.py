35933f3f805c2e5f414ce62520e2167c6bd8e986
msanchez10 <maximiliano.sanchezv@outlook.com>
2021-08-19 18:31:00 -0400

    Se agtegar app personal, incluyendo urls,setting,views y templates

--- proyecto_test/urls.py
+++ proyecto_test/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('cliente.urls')), # ruta por defecto
     path('cliente/', include('cliente.urls')),
+    path('max_sanchez/',include('Max_Sanchez.urls')),
 ]
