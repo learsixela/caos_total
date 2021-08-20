77e9454097a965c59922a97094a487e42b3fd965
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-08-19 22:09:41 -0400

    modificando urls

--- cristiancuevas/models.py
+++ cristiancuevas/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-
-""" class Cliente(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    rut = models.CharField(max_length=12)
-    dv = models.CharField(max_length=1)
-"""
--- cristiancuevas/views.py
+++ cristiancuevas/views.py
@@ -1,29 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from .models import Cliente
+
 
 def inicio(request):
-    # return HttpResponse("this is the equivalent of @app.route('/')!")
-    return render(request, "index.html")
-
-""" def agregar(request):
-    # request.post['parametro']
-    # capturando los parametros enviados por el formulario
-    Cliente.objects.create(
-    nombre = request.POST['nombre'],
-    apellido = request.POST['apellido'],
-    rut = request.POST['rut'],
-    dv = request.POST['dv'],
-    )
-    return render(request, "index.html")
-
-def leer(request):
-    return render(request, "index.html")
-
-
-def actualizar(request):
-    return render(request, "index.html")
-
-
-def eliminar(request):
-    return render(request, "index.html") """
+    return HttpResponse("Esta es la aplicacion de israel XD")
 
