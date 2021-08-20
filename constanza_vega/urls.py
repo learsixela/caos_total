62c5076eea9e221ef6259c2579d2acdcdd6bff71
cvega <AvItala980>
2021-08-19 21:32:36 -0400

    intento de merge

--- constanza_vega/urls.py
+++ constanza_vega/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('pic', views.show_pic)
+    path('', views.index, name="index_cvega"),
+    path('pic', views.show_pic, name="pic_cvega"),
 ]
