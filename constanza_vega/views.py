34fea076d8661af090bffddea42b89776299b1b4
cvega <AvItala980>
2021-08-19 20:50:46 -0400

    respaldo

--- constanza_vega/views.py
+++ constanza_vega/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index_cvega.html')
+
+
+def show_pic(request):
+
+
+    return render(request, 'error_cvega.html')
