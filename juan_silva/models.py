72fe4ba200a128cd7333bc7c83ea7b9dc24f4dda
jsilva16 <juansilva.ambiental@gmail.com>
2021-08-19 21:24:59 -0400

    jsilva update

--- juan_silva/models.py
+++ juan_silva/models.py
@@ -6,3 +6,5 @@ class Cliente_jsilva(models.Model):
     rut = models.CharField(max_length=12)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
