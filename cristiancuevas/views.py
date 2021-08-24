f644c2c55730ae0cb097f564384ab82c7de36405
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-08-23 22:51:15 -0400

    modificacion migracion

--- cristiancuevas/views.py
+++ cristiancuevas/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Jugador
 
-def inicio(request):
-    return render(request, 'index.html')
+def index(request):
+    """ return HttpResponse("app de Toshi") """
+    return render(request, "cristiancuevas/index.html")
+
 
 def crear(request):
-    return HttpResponse("crear")
+    # request.post['parametro']
+    # capturando los parametros enviados por el formulario
+    Jugador.objects.create(
+    player_name = request.POST['player_name'],
+    player_number = request.POST['player_number'],
+    player_team = request.POST['player_team']
+    )
+    return render(request, "cristiancuevas/crear.html")
 
 def actualizar(request):
     return HttpResponse("actualizar")
